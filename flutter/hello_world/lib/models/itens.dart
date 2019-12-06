8b28dd99ecbfe3ecbb6bfc35c44421c81a3b23d3
RafaelSilvaPereira <rafael.pereira@ccc.ufcg.edu.br>
2019-12-06 17:57:18 -0300

    First Commit

---
+++ flutter/hello_world/lib/models/itens.dart
@@ -0,0 +1,19 @@
+class Item {
+  var title;
+  var done;
+
+  Item({this.title, this.done});
+
+  Item.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    done = json['done'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['title'] = this.title;
+    data['done'] = this.done;
+
+    return data;
+  }
+}
